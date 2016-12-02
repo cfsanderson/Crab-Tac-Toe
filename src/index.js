85908fc4d4214a0baf03ab862590b10deb0330a0
Caleb Frost Sanderson <calebsanderson@gmail.com>
2016-12-01 19:08:27 -0500

    checking in

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const main = () => {
 
 document.addEventListener('DOMContentLoaded', main)
 
-if (module.hot) {
-  module.hot.dispose(() => window.location.reload())
-  module.hot.accept(err => console.error(err))
-}
+// if (module.hot) {
+//   module.hot.dispose(() => window.location.reload())
+//   module.hot.accept(err => console.error(err))
+// }
